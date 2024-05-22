9431d656c26f6ba18fe09d6c3b964f83d108c68c
unknown <bismaibrahim500@gmail.com>
2024-05-22 11:55:37 +0500

    basics of datatype

---
+++ Tasks/datatypes.js
@@ -0,0 +1,58 @@
+// numbers can  be written with or without decimals.they can also written in exponentiol or scienticfic notation
+
+//NUMBERS 
+//  without decimal
+var num1 = 42; 
+// with decimal
+ var num2 =24.56;
+ //exponent 
+ var num3= 2e5;
+ // scientific notation
+ var num4= 2e-5;
+
+// BOOLEAN
+
+ //Boolean can only have two values: TRUSE OR FALSE 
+ var a =true;
+ var b=false;
+
+// STRING 
+ // A string is written with quotes. it can be written using single '' or double "" quotes
+ var webpageNAme='elite_coding';
+ var webpageNAme="elite_coding";
+
+//SYMBOL 
+// A symbol is a unique and immutable data type and may be used as identifier for object
+// properties.To create a new primitive symbol you can simply write symbol() with an optional  string as its description
+ 
+
+var sym=Symbol("");
+var sym01=symbol("foo");
+
+
+
+//NULL 
+// in javascript NULL is nothing it is supposed to be something that doesn't exist
+var data =null;
+
+//UNDEFINED 
+
+//In javascript a variable without a value, undefined, it also any variable can be emptied by setting the value to unf=defined 
+
+var data= undefined
+
+// OBJECT 
+// It is a collection of properties 
+var car={color:"black",model:"X", mileage:30};
+
+
+
+
+
+//  assignment------
+//Q!# Create a variable my_num and assign it any number.
+var my_num=33;
+//create a variable my_boolean_value and assign it any boolean.
+var my_boolean_value=true;
+//create a variable my_str and assign it any string
+var myappname="elite_coding"
